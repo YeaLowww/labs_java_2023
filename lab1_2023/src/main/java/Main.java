d4ada020330923a02ad794a67857535f60d41d80
YeaLow <89252423+YeaLowww@users.noreply.github.com>
2023-10-04 17:33:38 +0300

    update

--- lab1_2023/src/main/java/Main.java
+++ lab1_2023/src/main/java/Main.java
@@ -1,36 +1,47 @@
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         short value = 1107;
-        int C2 = value%2;
+        int C2 = value % 2;
         System.out.println(C2);
-        int C3 = value%3;
+        int C3 = value % 3;
         System.out.println(C3);
-        int C5 = value%5;
+        int C5 = value % 5;
         System.out.println(C5);
-        int C7 = value%7;
+        int C7 = value % 7;
         System.out.println(C7);
 
-        int  C = C3;
+        int C = C3;
 
 
         short a = 1;
-        short b = 1;
-        short m = 2;
-        short n = 2;
+        short b = -100;
+        short m = -10;
+        short n = 3;
 
         short s = 0;
-            if (n < a || m < b || a < 0 || b < 0 || n < 0 || m < 0){
-                System.out.println("Wrong nums");
-            } else {
-            for (short i = (short) a; i <= (short)n; i++) {
-                int num1 = i - C;
-                short num = (short)num1;
+        if (n < a || m < b) {
+            System.out.println("Wrong nums");
+        } else {
+            for (short i = a; i <= n; i++) {
+                if (i == 0) {
+                    System.out.println("i cant be 0");
+                    return;
+                } else {
+                    int num1 = i - C;
+                    short num = (short) num1;
 
-                for (short j = (short)b; j <= m; j++) {
-                    s += (short) ((i % j) / num);
+                    for (short j = b; j <= m; j++) {
+                        if (j == 0) {
+                            System.out.println("j cant be 0");
+                            return;
+                        } else {
+                            s += (short) ((i % j) / num);
+                        }
+                    }
                 }
             }
             System.out.println("S = " + s);
-    }}
+        }
+    }
 }
